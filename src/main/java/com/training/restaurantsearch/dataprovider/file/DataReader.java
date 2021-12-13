8e574e11eab3b297f6b064a05c099899c5e682b3
Caio Albuquerque Melo <caio.alb.melo@gmail.com>
2021-12-12 23:24:00 -0300

    changes logic to read data from csv file

--- src/main/java/com/training/restaurantsearch/dataprovider/file/DataReader.java
+++ src/main/java/com/training/restaurantsearch/dataprovider/file/DataReader.java
@@ -4,18 +4,16 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class DataReader<T> {
 
     public List<T> read(String fileName, Class<T> type) throws IOException {
         Resource resource = new ClassPathResource(fileName);
-        File file = resource.getFile();
         List<T> items;
-        try (FileReader fileReader = new FileReader(file)) {
+        try (InputStreamReader fileReader = new InputStreamReader(resource.getInputStream())) {
             items = new CsvToBeanBuilder(fileReader).withType(type).build().parse();
         }
         return items;
